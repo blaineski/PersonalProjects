9426e50e81a58f226105a0a97fc0c90bfb8acb5f
blaineski <8457296+blaineski@users.noreply.github.com>
2017-12-17 10:20:51 +0000

    indent file close to allow loop to complete

--- randomQuizGenerator.py
+++ randomQuizGenerator.py
@@ -58,5 +58,5 @@ for quizNum in range(35):
         # write the answer key to a file
         answerKeyFile.write('%s. %s\n' % (questionNum + 1, 'ABCD'[
             answerOptions.index(correctAnswer)]))
-        quizFile.close()
-        answerKeyFile.close()
+quizFile.close()
+answerKeyFile.close()
